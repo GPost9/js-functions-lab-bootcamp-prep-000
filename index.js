6fd98547a0ed9c0935e9558decff247cbdb64f01
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-04 18:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // write your code below!
 function happyHolidays(string){return "Happy holidays!"}
+
 function happyHolidaysTo(name){return `Happy holidays, ${name}!`}
+
+function happyHolidays(string, name){ return `Happy ${holiday}, ${name}!`}
